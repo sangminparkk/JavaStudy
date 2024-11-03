0303eec289966f18e464dcf2bcb15476764e2977
chandler <parksm0529@gmail.com>
2024-11-03 22:12:03 +0900

    refactor) Do not create new file when file name is wrong

--- src/main/java/me/chandler/ch8/_Throws_4.java
+++ src/main/java/me/chandler/ch8/_Throws_4.java
@@ -6,15 +6,18 @@ import java.io.IOException;
 public class _Throws_4 {
 
     public static void main(String[] args) {
-        File newFile = createFile(args[0]);
-        System.out.println(newFile.getName() + " 파일이 성공적으로 생성되었습니다.");
-
+        try {
+            File newFile = createFile(args[0]);
+            System.out.println(newFile.getName() + " 파일이 성공적으로 생성되었습니다.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + " 다시 입력해주세요");
+        }
     }
 
-    private static File createFile(String fileName) {
+    private static File createFile(String fileName) throws Exception {
         // 리팩토링
         if (fileName == null || fileName.isBlank()) {
-            fileName = "제목없음.txt";
+            throw new Exception("파일 이름이 유효하지 않습니다.");
         }
 
         File file = new File(fileName);
